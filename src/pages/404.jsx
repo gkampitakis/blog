e789d777996f4b3e668bfcb8fdff922314e6a028
George Kampitakis <gkabitakis@gmail.com>
2020-04-13 11:32:06 +0100

    feat: implement 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../layout';
+import notFoundLogo from '../images/404.png';
 
 export default (props) => {
-	return <div>404 page not found</div>;
+	return (
+		<Layout>
+			<div className="pageNotFound container">
+				<div className="logo">
+					<span className="statusCode">4</span>
+					<img alt="Sorry about that" src={notFoundLogo} />
+					<span className="statusCode">4</span>
+				</div>
+				<h2>OOPS! PAGE NOT FOUND</h2>
+				<p className="description">
+					Sorry but the page you are looking for does not exist, has been removed. name changed or is
+					temporarily unavailable
+				</p>
+			</div>
+		</Layout>
+	);
 };
