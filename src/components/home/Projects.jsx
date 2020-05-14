3d02e6320d8cf7935739032438824be2679cc98b
gkampitakis <gkabitakis@gmail.com>
2020-05-14 22:48:42 +0100

    feat: working on projects component

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import emoji from 'react-easy-emoji';
 import GitHubButton from 'react-github-btn';
 import projects from '../../../data/projects';
+import Chip from '../Chip';
+import { isMobile } from 'react-device-detect';
 
 export default function Projects() {
 	return (
@@ -9,16 +11,21 @@ export default function Projects() {
 			<div className="title">
 				<h2>Projects {emoji('📚')}</h2>
 			</div>
-			<section>
-				{projects.map(({ icon, title, src, description, tags }) => (
+			<section className="projects">
+				{projects.map((
+					//TODO: fix colors for both modes
+					{ icon, title, src, description, tags }
+				) => (
 					<div className="projectItem" key={title}>
-						{emoji(icon)}
-						<h4>{title}</h4>
-						<h4>{description}</h4>
-						<GitHubButton href={src}>source</GitHubButton>
-						{tags.map((tag) => (
-							<span>{tag}</span>
-						))}
+						<div className="description">
+							{emoji(icon)}
+							<h4>{title}</h4>
+							<h5>{description}</h5>
+							<GitHubButton href={src}>source</GitHubButton>
+						</div>
+						<div className="tagList">
+							{tags.map((tag) => <Chip type="tag">{tag}</Chip>).slice(0, isMobile ? 3 : 10)}
+						</div>
 					</div>
 				))}
 			</section>
